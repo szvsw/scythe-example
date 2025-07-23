f3796bb0e16a76f456e4cd2936a6817d9d8c1f75
Sam Wolk <36545842+szvsw@users.noreply.github.com>
2025-07-23 11:06:06 -0400

    update field annotations

--- experiments/lifespan.py
+++ experiments/lifespan.py
@@ -1,22 +1,39 @@
 import numpy as np
 
+from pydantic import Field
 from scythe.base import ExperimentInputSpec, ExperimentOutputSpec, FileReference
 from scythe.registry import ExperimentRegistry
 
 
 class LifespanExperimentInputs(ExperimentInputSpec):
-    age: int
-    weight: float
-    coeffs: FileReference
+    age: int = Field(
+        default=..., description="The age of the subject [yrs]", ge=0, le=125
+    )
+    weight: float = Field(
+        default=..., description="The weight of the subject [lbs]", ge=0, le=500
+    )
+    coeffs: FileReference = Field(
+        default=..., description="The coefficients of the model"
+    )
 
 
 class LifespanExperimentOutputs(ExperimentOutputSpec):
-    lifespan: float
-    height: float
-    simulation_runtime: float
-    energy_used: float
-    memory_used: float
-    cpu_time: float
+    lifespan: float = Field(
+        default=..., description="The lifespan of the subject [yrs]"
+    )
+    height: float = Field(default=..., description="The height of the subject [cm]")
+    simulation_runtime: float = Field(
+        default=..., description="The runtime of the simulation [s]"
+    )
+    energy_used: float = Field(
+        default=..., description="The energy used by the simulation [J]"
+    )
+    memory_used: float = Field(
+        default=..., description="The memory used by the simulation [MB]"
+    )
+    cpu_time: float = Field(
+        default=..., description="The CPU time used by the simulation [s]"
+    )
 
 
 @ExperimentRegistry.Register()
